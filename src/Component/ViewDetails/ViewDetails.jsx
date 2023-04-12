ab83263edef25fade2b28ad17d4cb7afdb154d87
Md Sarowar <mdsarowarhang@gmail.com>
2023-04-12 06:06:44 +0300

    view detaic

--- src/Component/ViewDetails/ViewDetails.jsx
+++ src/Component/ViewDetails/ViewDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
-import {MapPinIcon,CurrencyDollarIcon,BriefcaseIcon } from '@heroicons/react/24/solid'
+import {CurrencyDollarIcon,BriefcaseIcon,EnvelopeIcon,PhoneIcon } from '@heroicons/react/24/solid'
 
 const ViewDetails = () => {
     const data = useParams();
@@ -14,7 +14,7 @@ const ViewDetails = () => {
     })
     const {
         job_description,job_responsibility,educational_requirements,experiences,
-        job_title,salary,contact_information,location
+        job_title,salary,phone,email,location
         } = details
     return (
         <div className=' mt-20'>
@@ -25,12 +25,16 @@ const ViewDetails = () => {
              <p className='bold m-2'>Educational Requirements:<span className="font-light">{educational_requirements}</span></p>
              <p className='bold m-2'>Experiences:<span className="font-light">{experiences}</span></p>
            </div>
-           <div className='card w-96 bg-fuchsia-100 ml-20 shadow-xl text-center'>
+           <div className='card w-96 bg-fuchsia-100 ml-20 shadow-xl text-center lg:mt-0 md:mt-10'>
             <div className="text-start mx-auto">
-                <h2 className='text-center bold'>Job Details</h2>
+                <h2 className='text-center bold mt-2'>Job Details</h2>
                 <hr className="text-neutral-900 m-2"/>
-                <h4 className='flex '><CurrencyDollarIcon className='w-6 h-6 text-indigo-600 mr-1'/>  Salary: <span>{salary}</span> </h4>
-                <h4 className='flex'> <BriefcaseIcon className='w-6 h-6 text-indigo-600 mr-1'/> Job title: <span>{job_title}</span></h4>
+                <h4 className='flex '><CurrencyDollarIcon className='w-6 h-6 text-indigo-600 mr-1'/>  Salary: <span className='ml-2'>{salary}</span> </h4>
+                <h4 className='flex'> <BriefcaseIcon className='w-6 h-6 text-indigo-600 mr-1'/> Job title: <span className='ml-2'>{job_title}</span></h4>
+                <h2 className='text-center bold mt-2'>Contact Information</h2>
+                <hr className='m-2 '/>
+                <h4 className='flex '><CurrencyDollarIcon className='w-6 h-6 text-indigo-600 mr-1'/> Phone: <span className='ml-2'>{phone}</span> </h4>
+                <h4 className='flex '><PhoneIcon className='w-6 h-6 text-indigo-600 mr-1'/> Email: <span className='ml-2'>{email}</span> </h4>
             </div>
            </div>
         </div>
